cba572be3b05ca0d51c647aa59b70bca3b5a9eb6
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-06-10 20:59:22 -0500

    NHibermate template

---
+++ Item/Visual C#/NHibernate/IQueryProcessorTemplate/IQueryProcessorTemplate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    public interface I$itemname$
+    {
+
+    }
+
+    public class $itemname$ : I$itemname$
+    {
+        private readonly ISession _session; 
+        private readonly IDateTime _dateTime;
+        private readonly IWebUserSession _userSession;
+
+        public $itemname$(ISession session, IDateTime dateTime, IWebUserSession userSession)
+        {
+            _session = session;
+            _userSession = userSession;
+            _dateTime = dateTime;
+        }
+
+    }
+}
